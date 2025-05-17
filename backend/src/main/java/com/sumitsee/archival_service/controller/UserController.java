d128180e0a4ff49efd6404cfb67848fca05022d8
Sum+it <sumitkamble35@gmail.com>
2025-05-18 02:23:03 +0530

    Update: Basic user operations

---
+++ backend/src/main/java/com/sumitsee/archival_service/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.sumitsee.archival_service.controller;
+
+import com.sumitsee.archival_service.model.User;
+import com.sumitsee.archival_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+//    @GetMapping("/ping")
+//    public String ping(){
+//        return "Backed in workng";
+//    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userService.getAllUsers();
+    }
+}
--- backend/src/main/java/com/sumitsee/archival_service/service/UserService.java
+++ backend/src/main/java/com/sumitsee/archival_service/service/UserService.java
@@ -1,4 +1,32 @@
 package com.sumitsee.archival_service.service;
 
+import com.sumitsee.archival_service.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@RequestMapping("users")
+@RequiredArgsConstructor
 public class UserService {
+//    private final UserService userService;
+
+    public List<User> getAllUsers(){
+        return Arrays.asList(
+                new User(1L, "Sumit", "sumit@example.com"),
+                new User(2L, "Sunil", "sunil@example.com")
+        );
+    }
+
+    public User getUserById(Long id){
+        return getAllUsers().stream()
+                .filter(user -> user.getId().equals((id)))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
