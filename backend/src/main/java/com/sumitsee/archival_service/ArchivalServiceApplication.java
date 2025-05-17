0bfdbc8f3c58f8693e17004f6b16ad7e422b3e01
Sum+it <sumitkamble35@gmail.com>
2025-05-18 01:22:59 +0530

    ADD: Scaffolded spring.io into backend (without db ATM)

---
+++ backend/src/main/java/com/sumitsee/archival_service/ArchivalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sumitsee.archival_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivalServiceApplication.class, args);
+	}
+
+}
